67e740dde19849246b7d3af2e652d9e354008e62
Dmitri54 <dmitrimining@gmail.com>
2023-08-02 20:49:41 +0700

    sem_01 EXCEPTIONS: different error handling priority.

--- sem_01/ExDemo.java
+++ sem_01/ExDemo.java
@@ -4,24 +4,31 @@ public class ExDemo {
     public static void main(String[] args) {
         int[] num = {4, 8, 16, 32, 64, 128,256, 512};
         int[] num2 = {2, 0, 4, 4, 0, 8};
-        for (int i = 0; i < num.length; i++) {
-            try{
-                System.out.println(num[i] + " / " + num2[i] + " = " + num[i]/num2[i]);
-            } catch (Error exception){
-                System.out.println("Попытка деления на ноль или выход за границы массива");
-            } // catch (ArrayIndexOutOfBoundsException exception){
-            //     System.out.println("Элемент не найден");
-            // }
-        }
+        try {
+            
+            for (int i = 0; i < num.length; i++) {
+                try{
+                    System.out.println(num[i] + " / " + num2[i] + " = " + num[i]/num2[i]);
+                } catch (ArithmeticException exception){
+                    System.out.println("Попытка деления на ноль или выход за границы массива");
+                } // Продолжает работать
+            } 
+        } catch (ArrayIndexOutOfBoundsException exception){
+            System.out.println("Элемент не найден");
+            System.out.println("Фатальная ошибка");
+        } // При попадании ошибки сюда, прекращает работу программы.
     }
 }
-// Throwable - super class, поэтому ловятся все исключения. 
 
-// Exception - является под классом Throwable. 
-// Обрабатывает исключения связанные только с програмными ошибками.
 
-// Error - Это ошибки которые возникают внешней среды
+// Чтобы сделать разный приоритет обработки ошибок.
+
 
 // 4 / 2 = 2
-// Exception in thread "main" java.lang.ArithmeticException: / by zero
-//         at sem_01.ExDemo.main(ExDemo.java:9)
+// Попытка деления на ноль или выход за границы массива
+// 16 / 4 = 4
+// 32 / 4 = 8
+// Попытка деления на ноль или выход за границы массива
+// 128 / 8 = 16
+// Элемент не найден
+// Фатальная ошибка
