42a2f152e24641935e6117513b1d4c8b47b6a227
Dmitri54 <dmitrimining@gmail.com>
2023-08-02 20:09:34 +0700

    sem_01 EXCEPTIONS: added a second block catch

--- sem_01/ExDemo.java
+++ sem_01/ExDemo.java
@@ -2,22 +2,26 @@ package sem_01;
 
 public class ExDemo {
     public static void main(String[] args) {
-        int[] num = {4, 8, 16, 32, 64, 128};
+        int[] num = {4, 8, 16, 32, 64, 128,256, 512};
         int[] num2 = {2, 0, 4, 4, 0, 8};
         for (int i = 0; i < num.length; i++) {
             try{
                 System.out.println(num[i] + " / " + num2[i] + " = " + num[i]/num2[i]);
             } catch (ArithmeticException exception){
                 System.out.println("Попытка деления на ноль");
+            } catch (ArrayIndexOutOfBoundsException exception){
+                System.out.println("Элемент не найден");
             }
         }
     }
 }
-// Вижу, что срабатывает блок try catch, но программа не падает, а продолжает работать.
-// 
+// Добавил второй catch - выводит второе собщение об выходе за границы массива.
+
 // 4 / 2 = 2
 // Попытка деления на ноль
 // 16 / 4 = 4
 // 32 / 4 = 8
 // Попытка деления на ноль
 // 128 / 8 = 16
+// Элемент не найден
+// Элемент не найден
