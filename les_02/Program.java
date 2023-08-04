59600b81fa777d6724a201c742fe1e56316c25eb
Dmitri54 <dmitrimining@gmail.com>
2023-08-04 16:43:38 +0700

    les_02 Exceptions: ClassCastException

--- les_02/Program.java
+++ les_02/Program.java
@@ -4,13 +4,20 @@
 
 package les_02;
 
+import java.io.File;
+
 public class Program {
     public static void main(String[] args) {
+// Exceptions:
+// 1) NullPointerException
+        // String name = null;
+        // System.out.println(name.length()); // NullPointerException
 
-// NullPointerException
-        String name = null;
-        System.out.println(name.length()); // NullPointerException
+// ClassCastException (Но у меня просто выводит Error) 
+        Object object = new String("123");
+        File file = (File) object;
+        System.out.println(file);
 
-        
+
     }
 }
