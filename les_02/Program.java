03d7e5a5715dc3f220c0ef303084d2fca0c457e6
Dmitri54 <dmitrimining@gmail.com>
2023-08-04 16:16:42 +0700

    les_02 Exceptions: NullPointerException

---
+++ les_02/Program.java
@@ -0,0 +1,16 @@
+// Урок 2. Исключения и их обработка
+// ---------------------------------------------------------------------------------
+
+
+package les_02;
+
+public class Program {
+    public static void main(String[] args) {
+
+// NullPointerException
+        String name = null;
+        System.out.println(name.length()); // NullPointerException
+
+        
+    }
+}
