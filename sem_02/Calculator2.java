32f0a6aa90bd455ce023a3b2f9454cdb42614d01
Dmitri54 <dmitrimining@gmail.com>
2023-08-05 14:21:10 +0700

    sem_02 Added file Calculator2 - this is example with Uncheckend Exception

---
+++ sem_02/Calculator2.java
@@ -0,0 +1,40 @@
+// Пример третий c Unchecked. 
+// 
+
+package sem_02;
+
+import java.util.regex.Pattern;
+
+public class Calculator2 {
+
+    private double arg1, arg2;
+
+    public void setArg1(String value) {
+        if (isValidNumber(value)) {
+            arg1 = Double.parseDouble(value);
+        } else {
+            throw new InvalidArgumentException("Invalid argument: " + value + " is not a valid number");
+        }
+    }
+
+    public void setArg2(String value) {
+        if (isValidNumber(value)) {
+            arg2 = Double.parseDouble(value);
+        } else {
+            throw new InvalidArgumentException("Invalid argument: " + value + " is not a valid number");
+        }
+    }
+
+    // ... остальной код класса ...
+
+    private boolean isValidNumber(String value) {
+        return Pattern.matches("^[-+]?\\d*\\.?\\d+$", value);
+    }
+
+    // Определяем собственное исключение внутри класса Calculator
+    public static class InvalidArgumentException extends RuntimeException {
+        public InvalidArgumentException(String message) {
+            super(message);
+        }
+    }
+}
