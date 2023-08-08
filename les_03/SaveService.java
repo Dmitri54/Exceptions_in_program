f1ed508925e16ebc1248c2d18d10180171fc26a6
Dmitri54 <dmitrimining@gmail.com>
2023-08-08 19:19:36 +0700

    les_03 Modified method save. Throwing an exception to a higher level2

--- les_03/SaveService.java
+++ les_03/SaveService.java
@@ -8,6 +8,9 @@ public class SaveService {
         System.out.println("save document started...");
         try (FileWriter writer = new FileWriter("/Users/User/Desktop/Exceptions_in_program/les_03/test3")) {
             throw new IOException("Operation failed");
-        }
+        } catch (IOException e){
+            System.out.println(e.getMessage()); // вывожу некое сообщение
+            throw e; // Пробрасываю его на уровень выше
+        } 
     }
 }
