d7870d07a4734bdf0c1b0ed745a56f83b1dc8383
Dmitri54 <dmitrimining@gmail.com>
2023-08-08 19:52:49 +0700

    les_03 Added a field for more information.

--- les_03/SaveService.java
+++ les_03/SaveService.java
@@ -2,15 +2,17 @@ package les_03;
 
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.Date;
 
 public class SaveService {
-    public void save() {
+    public void save() throws SavedException{
+        Date date = new Date();
         System.out.println("save document started...");
         try (FileWriter writer = new FileWriter("/Users/User/Desktop/Exceptions_in_program/les_03/test3")) {
             throw new IOException("Operation failed");
         } catch (IOException e){
             System.out.println(e.getMessage()); // вывожу некое сообщение
-            throw new SavedException("Saved document failed", e);
+            throw new SavedException("Saved document failed", date, e);
         } 
     }
 }
--- les_03/SavedException.java
+++ les_03/SavedException.java
@@ -1,6 +1,8 @@
 package les_03;
 
 import java.io.IOException;
+import java.util.Date;
+
 
 // public class SavedException extends IOException {
 //     public SavedException(String message, Exception e){
@@ -8,8 +10,14 @@ import java.io.IOException;
 //     }
 // }
 
-public class SavedException extends RuntimeException {
-    public SavedException(String message, Exception e){
-        super(message, e);
+
+public class SavedException extends IOException {
+
+    private Date startDate; // Добавил поле, для получения дополнительной информации.
+
+    public SavedException(String message, Date startDate, Exception e){
+        // super(message, e);
+        super ("exception with message: " + message, e); // Модифицировал. Пример заранее заготовленной фразы.
+        this.startDate = startDate;
     }
 }
