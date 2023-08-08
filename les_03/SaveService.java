b1761752a922e4263561471414824477ebf5abe7
Dmitri54 <dmitrimining@gmail.com>
2023-08-08 19:43:37 +0700

    les_03 if use RuntimeException

--- les_03/SaveService.java
+++ les_03/SaveService.java
@@ -4,7 +4,7 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class SaveService {
-    public void save() throws SavedException{ // Проброс на уровень выше
+    public void save() {
         System.out.println("save document started...");
         try (FileWriter writer = new FileWriter("/Users/User/Desktop/Exceptions_in_program/les_03/test3")) {
             throw new IOException("Operation failed");
--- les_03/SavedException.java
+++ les_03/SavedException.java
@@ -2,7 +2,13 @@ package les_03;
 
 import java.io.IOException;
 
-public class SavedException extends IOException {
+// public class SavedException extends IOException {
+//     public SavedException(String message, Exception e){
+//         super(message, e);
+//     }
+// }
+
+public class SavedException extends RuntimeException {
     public SavedException(String message, Exception e){
         super(message, e);
     }
