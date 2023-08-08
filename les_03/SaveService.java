40f424a6b84000874325ee7019660eabb1448c46
Dmitri54 <dmitrimining@gmail.com>
2023-08-08 19:40:34 +0700

    les_03 Throw in my exception

--- les_03/SaveService.java
+++ les_03/SaveService.java
@@ -4,13 +4,13 @@ import java.io.FileWriter;
 import java.io.IOException;
 
 public class SaveService {
-    public void save() throws IOException{ // Чтобы выбрасить исключение выше по стеку
+    public void save() throws SavedException{ // Проброс на уровень выше
         System.out.println("save document started...");
         try (FileWriter writer = new FileWriter("/Users/User/Desktop/Exceptions_in_program/les_03/test3")) {
             throw new IOException("Operation failed");
         } catch (IOException e){
             System.out.println(e.getMessage()); // вывожу некое сообщение
-            throw e; // Пробрасываю его на уровень выше
+            throw new SavedException("Saved document failed", e);
         } 
     }
 }
