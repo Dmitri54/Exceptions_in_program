677956c7a27579343be2f450beec59305f2b3d7e
Dmitri54 <dmitrimining@gmail.com>
2023-08-11 15:32:48 +0700

    hw_01 task 2 and task 3 not done yet

--- hw_01/Program3.java
+++ hw_01/Program3.java
@@ -1,9 +1,12 @@
 // // Задание 3:
 // ---------------------------------------------------------------------------------------------
-// Реализуйте метод divArrays, принимающий в качестве аргументов два целочисленных массива a и b, и возвращающий новый массив с, каждый элемент которого равен частному элементов двух входящих массивов в той же ячейке.
+// Реализуйте метод divArrays, принимающий в качестве аргументов два целочисленных массива a и b, 
+// и возвращающий новый массив с, каждый элемент которого равен частному элементов двух входящих 
+// массивов в той же ячейке.
 // Если длины массивов не равны - верните нулевой массив длины 1.
-// Важно: При выполнении метода единственное исключение, 
-// которое пользователь может увидеть - RuntimeException, т.е. ваше.
+
+// Важно: При выполнении метода единственное исключение, которое пользователь может увидеть 
+// RuntimeException, т.е. ваше.
 // Напишите свой код в методе divArrays класса Answer. 
 // Метод divArrays принимает на вход два параметра:
 
@@ -27,55 +30,116 @@ package hw_01;
 // }
 
 import java.util.Arrays;
+import java.util.Random;
 
-class Answer {
-    public int[] divArrays(int[] a, int[] b) {
-        // Введите свое решение ниже
-        // int[] c = new int[b.length];
-        // try {
-            
-        //     for (int i = 0; i < b.length; i++) {
-        //         c[i] = a[i] / b[i];
-        //     }
-        //     return c;
-            
-        // } catch (RuntimeException e) {
-        //     System.out.println("RuntimeException");
-        // }
-        // return c;
-
-        int[] c = new int[b.length];
-        if (a.length == b.length){
-            for (int i = 0; i < b.length; i++) {
-                c[i] = a[i] / b[i];
-            }
-            return c;
-
-        } else {
-            throw new RuntimeException();
-        }
-        
-
-    }
-}
-
-// Не удаляйте этот класс - он нужен для вывода результатов на экран и проверки
 public class Program3 {
     public static void main(String[] args) {
-        int[] a = {};
-        int[] b = {};
+        task();
+    }
 
-        if (args.length == 0) {
-            // При отправке кода на Выполнение, вы можете варьировать эти параметры
-            a = new int[] { 12, 8, 16 };
-            b = new int[] { 4, 2, 4 };
-        } else {
-            a = Arrays.stream(args[0].split(", ")).mapToInt(Integer::parseInt).toArray();
-            b = Arrays.stream(args[1].split(", ")).mapToInt(Integer::parseInt).toArray();
+    static void task() {
+        try {
+            int[] arr1 = new GenerateArray().generateArray();
+            int[] arr2 = new GenerateArray().generateArray();
+            int[] result = new ProcessArray().arraysDivide(arr1, arr2);
+            for (int a1 : arr1)
+                System.out.printf("%d\t", a1);
+            System.out.println();
+            for (int a2 : arr2)
+                System.out.printf("%d\t", a2);
+            System.out.println();
+            System.out.println("Частное элементов двух массивов равна:");
+            for (int i : result)
+                System.out.printf("%d\t", i);
+        } catch (ProcessArray.CustomArraySizeException e) {
+            System.out.println(e.getClass().getName());
+            System.out.println(e.getMessage());
+            System.out.printf(
+                    "Длина первого массива: %d\nДлина второго массива: %d\n",
+                    e.getLength1(), e.getLength2());
+        } catch (ProcessArray.CustomDivideException e) {
+            System.out.println(e.getClass().getName());
+            System.out.println(e.getMessage());
+            System.out.printf("Элемент по индексу %d равен нулю", e.getElement());
+        }
+    }
+
+    public class GenerateArray {
+        public int[] generateArray() {
+            Random random = new Random();
+            int[] arr = new int[random.nextInt(2) + 4];
+            for (int i = 0; i < arr.length; i++) {
+                arr[i] = random.nextInt(10);
+            }
+            return arr;
+        }
+    }
+
+    public class ProcessArray {
+        public int[] arraysDifference(int[] arr1, int[] arr2) {
+            if (arr1 == null || arr2 == null) {
+                throw new NullPointerException("Оба массива должны существовать.");
+            }
+            if (arr1.length != arr2.length) {
+                throw new CustomArraySizeException("Кол-во элементов массива должно быть одинаково.",
+                        arr1.length, arr2.length);
+            }
+            int[] result = new int[arr1.length];
+            for (int i = 0; i < arr1.length; i++) {
+                result[i] = arr1[i] - arr2[i];
+            }
+            return result;
         }
 
-        Answer ans = new Answer();
-        String itresume_res = Arrays.toString(ans.divArrays(a, b));
-        System.out.println(itresume_res);
+        public int[] arraysDivide(int[] arr1, int[] arr2) {
+            if (arr1 == null || arr2 == null) {
+                throw new NullPointerException("Оба массива должны существовать.");
+            }
+            if (arr1.length != arr2.length) {
+                throw new CustomArraySizeException("Кол-во элементов массива должно быть одинаково.",
+                        arr1.length, arr2.length);
+            }
+            int[] result = new int[arr1.length];
+            for (int i = 0; i < arr1.length; i++) {
+                if (arr2[i] == 0) {
+                    throw new CustomDivideException("Делить на ноль нинада !", i);
+                }
+                result[i] = arr1[i] / arr2[i];
+            }
+            return result;
+        }
+
+        class CustomArraySizeException extends RuntimeException {
+
+            int length1;
+            int length2;
+
+            public int getLength1() {
+                return length1;
+            }
+
+            public int getLength2() {
+                return length2;
+            }
+
+            public CustomArraySizeException(String message, int length1, int length2) {
+                super(message);
+                this.length1 = length1;
+                this.length2 = length2;
+            }
+        }
+
+        class CustomDivideException extends ArithmeticException {
+            int element;
+
+            public int getElement() {
+                return element;
+            }
+
+            public CustomDivideException(String message, int element) {
+                super(message);
+                this.element = element;
+            }
+        }
     }
 }
