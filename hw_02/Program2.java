9dca71aae5d506857e3fe35a6037866b5b1d6e05
Dmitri54 <dmitrimining@gmail.com>
2023-08-13 17:25:35 +0700

    hw_02 Added corrected teacher to the task 2

--- hw_02/Program2.java
+++ hw_02/Program2.java
@@ -8,7 +8,6 @@
 // которые наследуются от класса Exception. Это позволяет нам определить специфическую 
 // причину ошибки и передать информацию об ошибке для последующей обработки.
 
-
 package hw_02;
 
 import java.util.Scanner;
@@ -16,31 +15,34 @@ import java.util.Scanner;
 public class Program2 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        boolean continueInput = true;
+        do {
+            try {
+                System.out.print("Введите первое число: ");
+                int num1 = scanner.nextInt();
 
-        try {
-            System.out.print("Введите первое число: ");
-            int num1 = scanner.nextInt();
+                System.out.print("Введите второе число: ");
+                int num2 = scanner.nextInt();
 
-            System.out.print("Введите второе число: ");
-            int num2 = scanner.nextInt();
+                int result = DivisionNum(num1, num2);
+                System.out.println("Результат: " + result);
+                continueInput = false;
 
-            int result = DivisionNum(num1, num2);
-            System.out.println("Результат: " + result);
-
-        } catch (DivisionByZeroException e) {
-            System.out.println(e.getMessage());
-        }
+            } catch (DivisionByZeroException e) {
+                System.out.println(e.getMessage());
+            }
+        } while (continueInput);
     }
 
-    public static int DivisionNum (int a, int b) throws DivisionByZeroException {
-        if (b == 0){
+    public static int DivisionNum(int a, int b) throws DivisionByZeroException {
+        if (b == 0) {
             throw new DivisionByZeroException("Деление на ноль недопустимо");
         }
         return a / b;
     }
 
     public static class DivisionByZeroException extends Exception {
-        public DivisionByZeroException (String message){
+        public DivisionByZeroException(String message) {
             super(message);
         }
     }
